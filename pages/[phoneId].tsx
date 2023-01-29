882daaef6910f7ddb9ab7f5b9921423639fb7046
SIKORSKY YAROSLAV <mr.the.sikora@gmail.com>
2023-01-30 01:52:15 +0200

    descriptions

--- pages/[phoneId].tsx
+++ pages/[phoneId].tsx
@@ -5,8 +5,11 @@ import { GetStaticPaths, GetStaticProps, NextPage } from "next/types";
 import React from "react";
 import { addItem } from "../store/cart/cartSlice";
 import { CartItemType } from "../store/cart/types";
-import {  useAppDispatch } from '../store/store';
-import { fetchedData, fetchedDataForSearch } from '../types/fetchedDataForPages';
+import { useAppDispatch } from "../store/store";
+import {
+    fetchedData,
+    fetchedDataForSearch,
+} from "../types/fetchedDataForPages";
 
 interface PhoneIdProps {
     data: fetchedDataForSearch;
@@ -60,50 +63,118 @@ const PhoneId: NextPage<PhoneIdProps> = ({ data }) => {
                             <div className="phoneDesc__top-line" />
                             <div className="phoneDesc__top-body-btn">
                                 <Link href="#phoneDesc">
-                                    <Button gradientMonochrome="info" size={"xl"}>
+                                    <Button
+                                        gradientMonochrome="info"
+                                        size={"xl"}
+                                    >
                                         ХАРАКТЕРИСТИКИ
                                     </Button>
                                 </Link>
-                                <Button gradientMonochrome="success" size={"xl"} id="phoneDesc" onClick={handleBuy}>
+                                <Button
+                                    gradientMonochrome="success"
+                                    size={"xl"}
+                                    id="phoneDesc"
+                                    onClick={handleBuy}
+                                >
                                     КУПИТИ
                                 </Button>
                             </div>
                         </div>
                     </div>
-                    <div className="phoneDesc__bottom" >
-                        <h2 className="phoneDesc__bottom-desc" >
-                            Характеристики
+                    <div className="phoneDesc__bottom">
+                        <h2 className="phoneDesc__bottom-desc">
+                            Основні характеристики:
                         </h2>
-                        <div>
-                            <h3>Камера:</h3>
-                            <p>{data.description.camera}</p>
+                        <div className="phoneDesc__bottom-field">
+                            <div className="phoneDesc__bottom-field-title">
+                                Камера
+                            </div>
+                            <div className="phoneDesc__bottom-field-dotes">
+                                ...............................................................................................................................................................................................................................
+                            </div>
+                            <div className="phoneDesc__bottom-field-text">
+                                <ul className="phoneDesc__bottom-field-list">
+                                    {data.description.camera
+                                        .split(";")
+                                        .map((value, index) => {
+                                            return (
+                                                <li key={index}>{value};</li>
+                                            );
+                                        })}
+                                </ul>
+                            </div>
                         </div>
-                        <div>
-                            <h3>Дисплей:</h3>
-                            <p>{data.description.display}</p>
+                        <div className="phoneDesc__bottom-field">
+                            <div className="phoneDesc__bottom-field-title">
+                                Дисплей
+                            </div>
+                            <div className="phoneDesc__bottom-field-dotes">
+                                ...............................................................................................................................................................................................................................
+                            </div>
+                            <div className="phoneDesc__bottom-field-text">
+                                <ul className="phoneDesc__bottom-field-list">
+                                    {data.description.display
+                                        .split(";")
+                                        .map((value, index) => {
+                                            return (
+                                                <li key={index}>{value};</li>
+                                            );
+                                        })}
+                                </ul>
+                            </div>
                         </div>
-                        <div>
-                            <h3>Батарея:</h3>
-                            <p>{data.description.battery}</p>
+                        <div className="phoneDesc__bottom-field">
+                            <div className="phoneDesc__bottom-field-title">
+                                Батарея
+                            </div>
+                            <div className="phoneDesc__bottom-field-dotes">
+                                ...............................................................................................................................................................................................................................
+                            </div>
+                            <div className="phoneDesc__bottom-field-text">
+                                {data.description.battery}
+                            </div>
                         </div>
-                        <div>
-                            <h3>Процесор:</h3>
-                            <p>{data.description.processor}</p>
+                        <div className="phoneDesc__bottom-field">
+                            <div className="phoneDesc__bottom-field-title">
+                                Процесор
+                            </div>
+                            <div className="phoneDesc__bottom-field-dotes">
+                                ...............................................................................................................................................................................................................................
+                            </div>
+                            <div className="phoneDesc__bottom-field-text">
+                                {data.description.processor}
+                            </div>
                         </div>
-                        <div>
-                            {data.description.protection && (
-                                <>
-                                    <h3>Захист:</h3>
-                                    <p>{data.description.protection}</p>
-                                </>
-                            )}
+                        <div className="phoneDesc__bottom-field">
+                            <div className="phoneDesc__bottom-field-title">
+                                Розміри
+                            </div>
+                            <div className="phoneDesc__bottom-field-dotes">
+                                ...............................................................................................................................................................................................................................
+                            </div>
+                            <div className="phoneDesc__bottom-field-text">
+                                <ul className="phoneDesc__bottom-field-list">
+                                    <li>Довжина: {data.description.length}</li>
+                                    <li>Товщина: {data.description.thickness}</li>
+                                    <li>Ширина: {data.description.width}</li>
+                                </ul>
+                            </div>
                         </div>
-                        <div>
-                            <h3>Розміри:</h3>
-                            <p>Довжина: {data.description.length}</p>
-                            <p>Товщина: {data.description.thickness}</p>
-                            <p>Ширина: {data.description.width}</p>
-                        </div>
+                        {data.description.protection && (
+                            <>
+                                <div className="phoneDesc__bottom-field">
+                                    <div className="phoneDesc__bottom-field-title">
+                                        Захист
+                                    </div>
+                                    <div className="phoneDesc__bottom-field-dotes">
+                                        ...............................................................................................................................................................................................................................
+                                    </div>
+                                    <div className="phoneDesc__bottom-field-text">
+                                        {data.description.protection}
+                                    </div>
+                                </div>
+                            </>
+                        )}
                     </div>
                 </div>
             </div>
@@ -113,12 +184,9 @@ const PhoneId: NextPage<PhoneIdProps> = ({ data }) => {
 
 export default PhoneId;
 
-
 export const getStaticProps: GetStaticProps = async (ctx: any) => {
     const { params } = ctx;
-    const res = await fetch(
-        "https://next-phones.vercel.app/api/next-phones"
-    );
+    const res = await fetch("https://next-phones.vercel.app/api/next-phones");
     const dataPhone: fetchedData[] = await res.json();
     const nestedArray = dataPhone.map((phone) =>
         phone.colors.map((phoneByColor, index) => {
@@ -137,8 +205,7 @@ export const getStaticProps: GetStaticProps = async (ctx: any) => {
 
     const dataArr = nestedArray.flat();
 
-    const data = dataArr.find((obj) => obj.id === params.phoneId)
-
+    const data = dataArr.find((obj) => obj.id === params.phoneId);
 
     return {
         props: {
@@ -148,9 +215,7 @@ export const getStaticProps: GetStaticProps = async (ctx: any) => {
 };
 
 export const getStaticPaths: GetStaticPaths = async () => {
-    const res = await fetch(
-        "https://next-phones.vercel.app/api/next-phones"
-    );
+    const res = await fetch("https://next-phones.vercel.app/api/next-phones");
     const dataPhone: fetchedData[] = await res.json();
     const nestedArray = dataPhone.map((phone) =>
         phone.colors.map((phoneByColor, index) => {
@@ -181,4 +246,3 @@ export const getStaticPaths: GetStaticPaths = async () => {
         fallback: true,
     };
 };
-
